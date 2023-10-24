98e7e448600a709a87d06f4e6e37d3143b7f8b00
James369963 <jamesjames369963@gmail.com>
2023-10-24 08:52:04 +0700

    add header component

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   }, []);
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />} />
